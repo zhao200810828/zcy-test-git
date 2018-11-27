694bbeb11d9fb195b92dcafa3f51510b790e5897
zhaocy6 <zhaocy6@asiainfo.com>
2018-11-27 14:09:26 +0800

    测试如何将本地maven项目上传到远程git

---
+++ src/main/java/com/zcy/test/Application.java
@@ -0,0 +1,10 @@
+package com.zcy.test;
+
+/**
+ * Created by sue on 2018/11/27.
+ */
+public class Application {
+    public static void main(String[] args){
+        System.out.println("kkkk");
+    }
+}
